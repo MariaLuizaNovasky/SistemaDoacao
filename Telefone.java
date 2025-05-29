02839cb00ac53968a6fa4719a07b2b959d763f7a
Jorge jorge neto <143630429+Jorge0708@users.noreply.github.com>
2025-05-28 22:30:43 -0300

    Adiciona classe de apoio Telefone
    A classe Telefone representa os dados de contato com atributos para DDD, número e tipo (ex: Celular, Fixo, Comercial).
    Inclui construtor, getters, setters e método toString.
    Será utilizada para compor os dados de Pessoa Física ou Jurídica.

---
+++ Telefone.java
@@ -0,0 +1,48 @@
+package SistemaDoacao;
+
+public class Telefone {
+    private String ddd;
+    private String numero;
+    private String tipo; 
+
+  
+    public Telefone() {}
+
+   
+    public Telefone(String ddd, String numero, String tipo) {
+        this.ddd = ddd;
+        this.numero = numero;
+        this.tipo = tipo;
+    }
+
+    
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "(" + ddd + ") " + numero + " - " + tipo;
+    }
+}
